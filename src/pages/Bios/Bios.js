879cb41df4b7e60f737815694b962e466ce90c4a
francishui <monkeytech2024@gmail.com>
2024-11-24 18:12:41 -0500

    bios

--- src/pages/Bios/Bios.js
+++ src/pages/Bios/Bios.js
@@ -7,6 +7,7 @@ import {} from "@mui/material";
 import {InstagramEmbed } from 'react-social-media-embed';
 import WhatsappIcon from "@mui/icons-material/WhatsApp";
 import { useNavigate, useParams } from "react-router-dom";
+import {Chip} from '@mui/material';
 import ADs from "../../assets/data/bioAD.json";
 import BDs from "../../assets/data/bioBD.json";
 import Presidents from "../../assets/data/bioPresident.json";
@@ -80,7 +81,18 @@ if(bio ===null)
             </Grid>
             <Grid item lg={8} md={6} xs={12}>
               
-            <Typography sx={{fontWeight:"400", color:"primary.darkred", fontSize:{lg:"56px", md: "24px", xs:"18px"}}}>{bio.name}</Typography>
+            <Typography sx={{fontWeight:"400", color:"primary.darkred", marginBottom:{lg: "1.5vh", md: '2vh', xs:"2vh"}, fontSize:{lg:"56px", md: "24px", xs:"18px"}}}>{t(bio.name)}</Typography>
+            <Stack direction="row" spacing="10px" sx={{marginBottom:{lg: "1.5vh", md: '2vh', xs:"2vh"}}}>
+            {
+                bio.catergories.map((catergory) => {
+                  return(
+                    <Chip  sx={{height:{lg:"4vh", md:"3vw"}, fontSize:{lg:"24px", md: "24px", xs:"18px"}}} label={t(catergory)} />
+                  )
+                }
+              )}
+
+            </Stack>
+            <Typography sx={{fontWeight:"400", color:"primary.black", marginBottom:{lg: "1.5vh", md: '2vh', xs:"2vh"}, fontSize:{lg:"24px", md: "24px", xs:"18px"}}}>{t(bio.description)}</Typography>
         </Grid>
            </Grid>
         </Stack>
